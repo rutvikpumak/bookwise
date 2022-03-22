afd7df33f554d5a6dc58dcc64b08b4a6f790c273
Rutvik Umak <rutvikumak@gmail.com>
2022-03-22 11:11:30 +0530

    Create a new Remotion video

---
+++ src/backend/db/users.js
@@ -0,0 +1,19 @@
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * Every user will have likes (Likes are set to 0 by default), History Array, Playlists Array (added Watch Later Playlist in it by default) by default
+ * */
+
+export const users = [
+  {
+    _id: uuid(),
+    firstName: "Adarsh",
+    lastName: "Balika",
+    email: "adarshbalika@gmail.com",
+    password: "adarshBalika123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+];
